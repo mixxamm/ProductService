403427b9584085aad9e8475b5c941690b1a28b87
mixxamm <maximjanssensj5@gmail.com>
2020-01-20 10:38:23 +0100

    Initial commit

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ProductService { 
+   constructor() {}
+
+   getAllProducts(): Product[] {
+      let producten: Product[] = [];
+      producten.push(new Product("Product 1", "Merk 1", "Beschrijving 1", 10));
+      producten.push(new Product("Product 2", "Merk 2", "Beschrijving 2", 200));
+      return producten;
+    }
+}
